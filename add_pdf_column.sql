34a124bff2585814403dd06239fc3a70920d2b1e
Szubally Bruno <szubally.bruno@epistogram.com>
2025-08-08 14:54:34 +0200

    tisztitas

--- add_pdf_column.sql
+++
@@ -1,7 +0,0 @@
--- Add pdf_file_path column to forms table
-ALTER TABLE forms ADD COLUMN IF NOT EXISTS pdf_file_path TEXT;
-
--- Check if the column was added successfully
-SELECT column_name, data_type 
-FROM information_schema.columns 
-WHERE table_name = 'forms' AND column_name = 'pdf_file_path'; 
